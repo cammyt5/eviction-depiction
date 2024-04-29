d6675b2634cd869abee259fbcccb4e7aef668d7d
Cameron Dougal <cdougal@mit.edu>
2024-04-28 21:16:48 -0400

    wireframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    body {
+        max-width: 130ch;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    h1 {
+        text-align: center;
+        font-family: "Times New Roman", Times, serif;
+        font-size: 3em;
+    }
+
+    h2 {
+        text-align: center;
+        font-family: "Times New Roman", Times, serif;
+    }
+
+    p {
+        font-family: "Times New Roman", Times, serif;
+    }
+
+    p.names {
+        text-align: center;
+    }
+
+    hr {
+        border: 0;
+        height: 3px;
+    }
+
+    hr.blue {
+        background-image: linear-gradient(to right, rgba(0, 0, 0, 0), blue, rgba(0, 0, 0, 0));
+    }
+
+    hr.yellow {
+        background-image: linear-gradient(to right, rgba(0, 0, 0, 0), yellow, rgba(0, 0, 0, 0));
+
+    }
+</style>
+
+<body>
+    <hr class="blue"/><hr class="yellow"/>
+    <h1>The LLC Next Door</h1>
+    <h2><em>An analysis of corporate housing ownership (by corporation type) <br/>impacting eviction rates in Boston</em></h2>
+    <p class="names">By Cameron Dougal, Brian Zheng, Aleksandar Jovanovic-Hacon, and Max Katz-Christy</p>
+    <hr class="yellow"/><hr class="blue"/>
+
+    <h3>Background: Corporate Ownership on the Rise</h3>
+
+    <hr class="yellow"/>
+    <h3>Corporate Ownership ≠ Evictions</h3>
+
+    <hr class="blue"/>
+    <h3>Not All Corporations are Equal</h3>
+
+    <hr class="yellow"/>
+    <h3>Conclusion</h3>
+
+
+
+</body>
+
+
