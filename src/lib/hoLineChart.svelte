27cfccb54a310519a59d8c0ad82454ef774d9bc3
Brian Zheng <brianzheng205@gmail.com>
2024-05-10 20:24:47 -0400

    Create HO Line Chart

--- src/lib/hoLineChart.svelte
+++ src/lib/hoLineChart.svelte
@@ -1,111 +1,112 @@
 <script>
-    import * as d3 from "d3";
+  import * as d3 from "d3";
 
-    import metroBostonData from "../data/homeOwnership/metroB.js"
-    import suffolkData from "../data/homeOwnership/suffolkC.js"
-    import nationalData from "../data/homeOwnership/national.js"
-    
-    /**
-     * Want to create a line chart with the data from each json
-     * I want each line labeled and colored differently
-     * 
-     * On hover; I want to create a tooltip that displays the number
-     * at that year for each dataset
-     * 
-     * 
-     * 
-     * 
-    */
+  import { metroBostonData } from "../data/homeOwnership/metroB.js";
+  import { suffolkData } from "../data/homeOwnership/suffolkC.js";
+  import { nationalData } from "../data/homeOwnership/national.js";
 
-   // Define the dimensions and margins of the graph
-    const margin = { top: 20, right: 30, bottom: 30, left: 60 },
-        width = 800 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+  /**
+   * On hover: I want to create a tooltip that displays the number
+   * at that year for each dataset
+   */
 
-    // Append the SVG object to the body of the page
-    const svg = d3
-        .select("body")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  const transformData = (data) => {
+    return [...data.objects].map((d) => {
+      return {
+        year: d3.timeParse("%Y")(d.year),
+        rate: d.rate,
+      };
+    });
+  };
+  const dataCleaned = {
+    "Metro Boston": transformData(metroBostonData),
+    "Suffolk County": transformData(suffolkData),
+    National: transformData(nationalData),
+  };
+  const allData = Object.values(dataCleaned).flat();
 
-    // Parse the date/time
-    const parseYear = d3.timeParse("%Y");
-
-    // Set the ranges
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-
-    // Define the line
-    const valueline = d3
-        .line()
-        .x(function (d) {
-            return x(d.year);
-        })
-        .y(function (d) {
-            return y(d.rate); // Updated to 'rate'
-        });
-
-    // Format the data
-    const formatData = (data) => {
-        return data.objects.map((d) => ({
-            year: parseYear(d.year),
-            rate: d.rate // Updated to 'rate'
-        }));
-        };
-
-    // Load the data
-    const loadData = (data) => {
-        const formattedData = formatData(data);
-            // Scale the range of the data
-            x.domain(
-                d3.extent(formattedData, function (d) {
-                return d.year;
-                })
-            );
-        y.domain([
-            d3.min(formattedData, function (d) {
-            return d.rate;
-            }) - 5,
-            d3.max(formattedData, function (d) {
-            return d.rate;
-            }) + 5,
-        ]);
-
-        // Add the valueline path
-        svg
-            .append("path")
-            .data([formattedData])
-            .attr("class", "line")
-            .attr("d", valueline);
-
-        // Add the X Axis
-        svg
-            .append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        // Add the Y Axis
-        svg.append("g").call(d3.axisLeft(y));
-
-        // Add a title to the graph
-        svg
-            .append("text")
-            .attr("x", width / 2)
-            .attr("y", 0 - margin.top / 2)
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .text("Homeownership Rate Over Time");
+  // Define the dimensions and margins of the graph
+  const margin = { top: 20, right: 30, bottom: 30, left: 60 },
+    width = 800,
+    height = 400,
+    usableArea = {
+      top: margin.top,
+      right: width - margin.right,
+      bottom: height - margin.bottom,
+      left: margin.left,
     };
 
-    // Load data for each dataset
-    loadData(metroBostonData);
-    loadData(suffolkData);
-    loadData(nationalData);
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
 
+  let svg;
+  // scales
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(allData.map((d) => d.year)))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(allData.map((d) => d.rate)))
+    .range([usableArea.bottom - 20, usableArea.top])
+    .nice();
+  // axes
+  let xAxis, yAxis;
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale));
+  }
+
+  // Define the line
+  const valueLine = d3
+    .line()
+    .x((d) => xScale(d.year))
+    .y((d) => yScale(d.rate));
+  const colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+  $: {
+    for (const [label, data] of Object.entries(dataCleaned)) {
+      // line
+      const color = colors(label);
+      d3.select(svg)
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", color)
+        .attr("stroke-width", 1.5)
+        .attr("d", valueLine);
+
+      // label
+      d3.select(svg)
+        .append("text")
+        .attr("x", margin.left + usableArea.width)
+        .attr("y", yScale(data[data.length - 1].rate))
+        .attr("text-anchor", "left")
+        .style("font-size", "16px")
+        .style("fill", color)
+        .text(label);
+    }
+
+    // title
+    d3.select(svg)
+      .append("text")
+      .attr("x", margin.left + usableArea.width / 2)
+      .attr("y", 0)
+      .attr("text-anchor", "middle")
+      .style("font-size", "20px")
+      .text("Homeownership Rate Over Time");
+  }
 </script>
-<style>
 
+<svg viewBox="0 0 {width} {height}" bind:this={svg}>
+  <g transform="translate(0, {usableArea.height})" bind:this={xAxis} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+</svg>
+
+<style>
+  svg {
+    overflow: visible;
+    margin: 2em auto;
+  }
 </style>
