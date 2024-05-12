d6967d4641edc508ca49e60cb8c652fbfb579eed
Brian Zheng <brianzheng205@gmail.com>
2024-05-12 15:29:56 -0400

    Finalize HO Line Chart

--- src/lib/hoLineChart.svelte
+++ src/lib/hoLineChart.svelte
@@ -1,4 +1,5 @@
 <script>
+  import * as d3 from "d3";
   import * as Plot from "@observablehq/plot";
 
   import { metroBostonData } from "../data/homeOwnership/metroB.js";
@@ -6,35 +7,52 @@
   import { nationalData } from "../data/homeOwnership/national.js";
 
   let div;
+  const parseYear = d3.timeParse("%Y");
   const data = [
     ["Metro Boston", metroBostonData.objects],
     ["Suffolk County", suffolkData.objects],
     ["National", nationalData.objects],
-  ].flatMap(([Area, values]) => values.map((d) => ({ Area, ...d })));
+  ].flatMap(([Area, values]) =>
+    values.map((d) => ({
+      Area,
+      Year: parseYear(d.year),
+      Rate: d.rate,
+    }))
+  );
 
   $: {
     div?.firstChild?.remove(); // remove old chart, if any
 
     div?.append(
       Plot.plot({
-        style: "overflow: visible;",
+        style: "overflow: visible; width: 100%; height: 500px;",
         grid: true,
         marks: [
-          //Plot.ruleY([0]),  // start y-axis at 0
+          Plot.axisX({ label: "Year" }),
+          Plot.axisY({ label: "Rate (%)" }),
           Plot.lineY(data, {
-            x: "year",
-            y: "rate",
+            x: "Year",
+            y: "Rate",
             stroke: "Area",
-            tip: true,
+            tip: {
+              format: {
+                Area: true,
+                x: "%Y",
+                y: (d) => d3.format(".1%")(d / 100),
+              },
+              fontSize: 12,
+            },
           }),
           Plot.text(
             data,
             Plot.selectLast({
-              x: "year",
-              y: "rate",
+              x: "Year",
+              y: "Rate",
               z: "Area",
               text: "Area",
               textAnchor: "start",
+              fill: "Area",
+              fontSize: 12,
               dx: 10,
             })
           ),
